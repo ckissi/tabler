9fa438e253d0a506b0bb3c1b620ecd552b5f2c7f
Csaba Kissi <ckissi@gmail.com>
2019-05-21 22:54:07 +0200

    user

---
+++ resources/lang/en/user.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'profile' => 'Profile',
+    'settings' => 'Settings',
+    'logout' => 'Log Out',
+    'user' => 'User'
+];
--- resources/views/_partials/user.blade.php
+++ resources/views/_partials/user.blade.php
@@ -1,9 +1,19 @@
 <div class="dropdown">
     <a href="#" class="nav-link pr-0 leading-none" data-toggle="dropdown">
-        <span class="avatar"></span>
+        @auth
+            <img class="avatar" src="http://www.gravatar.com/avatar/{{ md5(strtolower(trim(Auth::user()->email))) }}?s=60">
+        @elseauth
+            <span class="avatar"></span>
+        @endauth
         <span class="ml-2 d-none d-lg-block">
             <span class="text-default">{{ $user->username }}</span>
-            <small class="text-muted d-block mt-1">@lang('tabler::user.user')</small>
+            <small class="text-muted d-block mt-1">
+             @auth
+                {{ Auth::user()->name }}
+             @elseauth()
+                @lang('tabler::user.user')
+             @endauth
+            </small>
         </span>
     </a>
     <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
@@ -13,8 +23,11 @@
         <a class="dropdown-item" href="{!! url(config('tabler.urls.settings')) !!}">
             <i class="dropdown-icon fe fe-settings"></i> @lang('tabler::user.settings')
         </a>
-        <a class="dropdown-item" href="javascript:void(0)" id="logout-button">
+        <a class="dropdown-item" href="javascript:void(0)" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" id="logout-button">
             <i class="dropdown-icon fe fe-log-out"></i> @lang('tabler::user.logout')
         </a>
+        <form id="logout-form" action="{!! url(config('tabler.urls.logout')) !!}" method="POST" style="display: none;">
+            @csrf
+        </form>
     </div>
 </div>
